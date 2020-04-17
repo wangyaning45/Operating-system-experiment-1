53a51c751d6b1ec38d1628367dafc2a881e69d04
vech1 <3237516232@qq.com>
2020-04-17 19:43:15 +0800

    first commit

---
+++ stoneM.asm
@@ -0,0 +1,173 @@
+; 程序源代码（stone.asm）
+; 本程序在文本方式显示器上从左边射出一个#号,以45度向右下运动，撞到边框后反射,如此类推.
+;  凌应标 2014/3
+;   NASM汇编格式
+    Dn_Rt equ 1                  ;D-Down,U-Up,R-right,L-Left
+    Up_Rt equ 2                  ;
+    Up_Lt equ 3                  ;
+    Dn_Lt equ 4                  ;
+    delay equ 50000					; 计时器延迟计数,用于控制画框的速度
+    ddelay equ 580					; 计时器延迟计数,用于控制画框的速度
+
+    org 07c00h					; 程序加载到07c00h，可用于生成COM
+start:
+	;xor ax,ax					; AX = 0   程序加载到0000：100h才能正确执行
+      mov ax,cs
+	mov es,ax					; ES = 0
+	mov ds,ax					; DS = CS
+	mov es,ax					; ES = CS
+	mov	ax,0B800h				; 文本窗口显存起始地址
+	mov	gs,ax					; GS = B800h
+      mov byte[char],'#'
+loop1:
+	dec word[count]				; 递减计数变量
+	jnz loop1					; >0：跳转;
+	mov word[count],delay
+	dec word[dcount]				; 递减计数变量
+      jnz loop1
+	mov word[count],delay
+	mov word[dcount],ddelay
+
+      mov al,1
+      cmp al,byte[rdul]
+	jz  DnRt
+      mov al,2
+      cmp al,byte[rdul]
+	jz  UpRt
+      mov al,3
+      cmp al,byte[rdul]
+	jz  UpLt
+      mov al,4
+      cmp al,byte[rdul]
+	jz  DnLt
+      jmp $	
+
+DnRt:
+	inc word[x]
+	inc word[y]
+	mov bx,word[x]
+	mov ax,25
+	sub ax,bx
+      jz  dr2ur
+	mov bx,word[y]
+	mov ax,79
+	sub ax,bx
+      jz  dr2dl
+	jmp show
+dr2ur:
+      mov word[x],23
+      mov byte[rdul],Up_Rt	
+      jmp show
+dr2dl:
+      mov word[y],77
+      mov byte[rdul],Dn_Lt	
+      jmp show
+
+UpRt:
+	dec word[x]
+	inc word[y]
+	mov bx,word[y]
+	mov ax,79
+	sub ax,bx
+      jz  ur2ul
+	mov bx,word[x]
+	mov ax,-1
+	sub ax,bx
+      jz  ur2dr
+	jmp show
+ur2ul:
+      mov word[y],77
+      mov byte[rdul],Up_Lt	
+      jmp show
+ur2dr:
+      mov word[x],1
+      mov byte[rdul],Dn_Rt	
+      jmp show
+
+	
+	
+UpLt:
+	dec word[x]
+	dec word[y]
+	mov bx,word[x]
+	mov ax,-1
+	sub ax,bx
+      jz  ul2dl
+	mov bx,word[y]
+	mov ax,-1
+	sub ax,bx
+      jz  ul2ur
+	jmp show
+
+ul2dl:
+      mov word[x],1
+      mov byte[rdul],Dn_Lt	
+      jmp show
+ul2ur:
+      mov word[y],1
+      mov byte[rdul],Up_Rt	
+      jmp show
+
+	
+	
+DnLt:
+	inc word[x]
+	dec word[y]
+	mov bx,word[y]
+	mov ax,-1
+	sub ax,bx
+      jz  dl2dr
+	mov bx,word[x]
+	mov ax,25
+	sub ax,bx
+      jz  dl2ul
+	jmp show
+
+dl2dr:
+      mov word[y],1
+      mov byte[rdul],Dn_Rt	
+      jmp show
+	
+dl2ul:
+      mov word[x],23
+      mov byte[rdul],Up_Lt	
+      jmp show
+	
+show:
+	
+	xor ax,ax	
+	mov ax, BootMessage
+	mov bp, ax; ES:BP = 串地址
+	mov cx, 18; CX = 串长度
+	mov ax, 01301h; AH = 13, AL = 01h
+	mov bx, 0082h; 页号为0(BH = 0) 闪烁绿字(BL = 0Ch,高亮)
+	mov dl, 10
+	mov dh,10
+	int 10h; 10h 号中断
+
+      	xor ax,ax                 ; 计算显存地址
+      	mov ax,word[x]
+	mov bx,80
+	mul bx
+	add ax,word[y]
+	mov bx,2
+	mul bx
+	mov bp,ax
+	mov ah,02h			;  0000：黑底、1111：亮白字（默认值为07h）
+	mov al,byte[char]			;  AL = 显示字符值（默认值为20h=空格符）
+	mov bx,0003h
+	mov word[gs:bp],ax  		;  显示字符的ASCII码值
+	jmp loop1
+	
+end:
+    jmp $                   ; 停止画框，无限循环 
+	
+datadef:	
+    count dw delay
+    dcount dw ddelay
+    rdul db Dn_Rt         ; 向右下运动
+    x    dw 0
+    y    dw 0
+    char db '#'
+    BootMessage: db "wangyaning17341152"
+
